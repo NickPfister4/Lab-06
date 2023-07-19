d76ddd2fbbbc7382357a42f9b5cec62f3f08d6e9
Nicholas Pfister <nicholaspfister@Nicholass-MacBook-Air.local>
2023-07-19 17:33:04 -0400

    accidentally added own decoder file reverting back to just encoder

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
+# Nicholas Pfister
 def encode(password):
     encoded_password = ""
     for digit in password:
-        new_digit = str((int(digit) + 3) % 10)  # Making sure to not go above 9 and create extra digits in the password
+        new_digit = str((int(digit) + 3) % 10)  #  making sure to not go above 9 and create extra digits
         encoded_password += new_digit
     return encoded_password
-
-
-def decode(password):
-    decoded_password = ""
-    for digit in password:
-        new_digit = str((int(digit) - 3) % 10)  # Making sure to not go below 0
-        decoded_password += new_digit
-    return decoded_password
-
-
-password = None
-encoded_password = None
 
 while True:
     print("Menu")
@@ -26,21 +15,13 @@ while True:
 
     option = input("Please enter an option: ")
 
-    if option == "1":
-        if password is not None:  # So program doesn't re-ask for password if it has already been given
-            print(f"The the original password is {password}, and the encoded password is {encoded_password}.")
-        else:
-            password = input("Please enter your password to encode: ")  # Entering original password
-            encoded_password = encode(password)
-            print("Your password has been encoded and stored!")
 
+    if option == "1": # Encode Password
+        password = (input("Please enter your password to encode: "))  # Original Password
+        encoded_password = encode(password)
+        print("Your password has been encoded and stored!")
     elif option == "2":
-        if encoded_password is not None:  # Same as above
-            print(f"The encoded password is {encoded_password}, and the original password is {password}.")
-        else:
-            encoded_password = input("Please enter your encoded password: ")  # Entering changed password
-            password = decode(encoded_password)
-            print("Your encoded password has been decoded and stored!")
+        print(f"The encoded password is {encoded_password}, and the original password is {password}.")
     elif option == "3":
         break
     else:
