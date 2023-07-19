fb09bc91cda0854ce13cee601abf1a544623563a
Nicholas Pfister <nicholaspfister@Nicholass-MacBook-Air.local>
2023-07-19 16:51:29 -0400

    couple fixes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 def encode(password):
     encoded_password = ""
     for digit in password:
-        new_digit = str((int(digit) + 3) % 10)  #making sure to not go above 9 and create extra digits
+        new_digit = str((int(digit) + 3) % 10)  # making sure to not go above 9 and create extra digits
         encoded_password += new_digit
     return encoded_password
 
@@ -16,8 +16,8 @@ while True:
 
     option = input("Please enter an option: ")
 
-    if option == "1": # Encode Password
-        password = (input("Please enter your password to encode: ")) # Original Password
+    if option == "1":  # Encode Password
+        password = (input("Please enter your password to encode: "))  # Original Password
         encoded_password = encode(password)
         print("Your password has been encoded and stored!")
     elif option == "2":
